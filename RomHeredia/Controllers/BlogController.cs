33a7da5863f3c286c8da40bd7b143a3f8e1d1a2c
romheredia <romeric.heredia@outlook.com>
2018-02-26 16:37:49 +1300

    Bootstrap 4 Integration

--- RomHeredia/Controllers/BlogController.cs
+++ RomHeredia/Controllers/BlogController.cs
@@ -44,6 +44,12 @@ namespace RomHeredia.Controllers
             return View(blogPost);
         }
 
+        // GET: Blog/Create
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
         // POST: Blog/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
